ca592af40c0e54b00b2cc41b191ccb250f7f7a57
Dhara Panchal <dhara.panchal@einfochips.com>
2020-04-27 12:34:25 +0530

    create action for message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
-
   end
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user_id
+      session[:user_id] = user.id
       flash[:success] = "Login successfully"
       redirect_to root_path
     else
@@ -20,6 +20,15 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 
 end
